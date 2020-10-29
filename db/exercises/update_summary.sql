69de5affcd4ee3731717661fc64b71df580cc530
Todd Norton <toddmnorton@live.com>
2020-10-29 14:17:53 -0600

    put request done

--- db/exercises/update_summary.sql
+++ db/exercises/update_summary.sql
@@ -1,6 +1,3 @@
 update exercises
-set summary = $1
-where exercise_id = $2;
-
-select user_id, activity, duration, distance from exercises
-where exercise_id = $2;
+set summary = ${summary}
+where exercise_id = ${exercise_id};
