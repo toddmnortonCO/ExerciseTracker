821a228d47216ed4214521c397f5bbc57a98fbf1
Todd Norton <toddmnorton@live.com>
2020-11-02 11:24:22 -0700

    working on stripe

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 const express = require('express');
 const app = express();
 app.use(express.static('.'));
-const YOUR_DOMAIN = 'http://localhost:4040/';
+const YOUR_DOMAIN = 'http://localhost:4040/Dashboard';
 app.post('/create-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
